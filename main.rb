57636e54ccd7b034a21a2ec34b9c4c921c462fc0
Regi Fronda <regisfronda@gmail.com>
2021-07-19 15:14:41 -0700

    Add Node class

--- main.rb
+++ main.rb
@@ -2,12 +2,20 @@
   # Get the Middle of the array and make it root.
   # Recursively do same for left half and right half.
       # Get the middle of left half and make it left child of the root
-        # created in step 1.
-      # Get the middle of right half and make it right child of the
-        # root created in step 1.
+      # Get the middle of right half and make it right child of the root
 
 # Node class with attributes data, left, right
+class Node
+  attr_accessor :data, :left, :right
   # include Comparable module
+  include Comparable
+
+  def initialize(data = nil, left = nil, right = nil)
+    @data = data
+    @left = left
+    @right = right
+  end
+end
 
 # Tree class
   # initialize with an array
