f7670b48024a0855e04be7fbbaf69ad329ed2b87
Regi Fronda <regisfronda@gmail.com>
2021-07-20 12:27:35 -0700

    Add prey-byebug

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-require 'node'
-require 'tree'
+require_relative 'node'
+require_relative 'tree'
 require 'pry-byebug'
+
 test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 test.build_tree(test.array)
