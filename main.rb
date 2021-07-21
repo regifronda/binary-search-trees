a493f3c9b093a5c3b0da0271e48216fa7410d9f7
Regi Fronda <regisfronda@gmail.com>
2021-07-21 12:05:19 -0700

    Add array of numbers 1 through 9 for easier testing and added call to insert method

--- main.rb
+++ main.rb
@@ -2,5 +2,8 @@ require_relative 'node'
 require_relative 'tree'
 require 'pry-byebug'
 
-test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+# test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+test = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+test.pretty_print
+test.insert(10)
 test.pretty_print
