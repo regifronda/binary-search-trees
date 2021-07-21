6848b3f3d8bc874d0e8e1f1ec92a02143e2f3414
Regi Fronda <regisfronda@gmail.com>
2021-07-21 01:09:16 -0700

    Add pretty_print method and changes so build_tree constructs a balanced binary search tree

--- tree.rb
+++ tree.rb
@@ -50,4 +50,10 @@ class Tree
     p "root.data before return root: #{root.data}"
     return root
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
