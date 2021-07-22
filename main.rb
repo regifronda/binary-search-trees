02f6ee087f478c163a46a78a6a27ca6f8a3bfeea
Regi Fronda <regisfronda@gmail.com>
2021-07-21 17:22:52 -0700

    Add call to delete method

--- main.rb
+++ main.rb
@@ -5,5 +5,5 @@ require 'pry-byebug'
 # test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 test = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
 test.pretty_print
-test.insert(10)
+test.delete(3)
 test.pretty_print
