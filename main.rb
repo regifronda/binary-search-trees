f0695768cc3393dd92d39ccb81290ac79e00d7bd
Regi Fronda <regisfronda@gmail.com>
2021-07-28 01:01:30 -0700

    Add balanced?, preorder, postorder, inorder, insert, and rebalance calls

--- main.rb
+++ main.rb
@@ -2,9 +2,18 @@ require_relative 'node'
 require_relative 'tree'
 require 'pry-byebug'
 
-# test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-test = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
-test.pretty_print
-test.delete(7)
-test.pretty_print
-test.level_order
+bst = Tree.new((Array.new(15) { rand(1..100) }))
+bst.pretty_print
+p bst.balanced?
+bst.preorder
+bst.postorder
+bst.inorder
+[150, 151, 133, 162, 199].each { |value| bst.insert(value) }
+bst.pretty_print
+p bst.balanced?
+bst.rebalance
+bst.pretty_print
+p bst.balanced?
+bst.preorder
+bst.postorder
+bst.inorder
